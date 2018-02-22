9a20a44d53ca727a35be4754bf41ebc8cdaa0d14
Marie Nabors <marienabors@thefirstacademy.org>
2018-02-22 01:01:21 -0500

    apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,35 +9,36 @@ module.exports = function (app){
 
 	app.post("/api/friends", function(req, res){
 		
-		var topMatch = {
+var topMatch = {
 			name: "",
 			photo: "",
 			friendDiff: 1000
 		};
 
-		console.log("req.body");
+	console.log("req.body");
 
 //stores user's input into req.body
-		var userInfo = req.body;
-		var userScores = userInfo.scores;
-		console.log("userScore");
-		console.log(usesrScores);
+var userInfo = req.body;
+var userScores = userInfo.scores;
+console.log("userScore");
+console.log(usesrScores);
 
 	//use subtraction to find the best match by comparing values from UserInput
-		var Differences = 0;
+var Differences = 0;
 
-		// loop thru database for best match
-		for (var i = 0; i<friends.length; i++) {
+// create a for loop to determine match by comparing user to all friends in array
+	for (var i = 0; i<friends.length; i++) {
 			console.log(friends[i]);
 			Differences = 0;
 
-			// for loop, loops through friends array
-			for (var j = 0; j<friends[i].scores[j]; j++){
+// for loop, loops through friends array
+//loop within a loop
+		for (var j = 0; j<friends[i].scores[j]; j++){
 	//math.abs (math absolute) ensures the use of a positive integer
 	//subtracts the scores from friends array from user's score to calculate the difference
 				Differences += Math.abs(parseInt(userScores[j])- parseInt(friends[i].scores[j]));
 //+= difference is equal to difference plus the result of math.abs...
-				//compaire for best match check all friends scores
+				//compaires friend differences for best match check all friends scores
 				if (difference <= topMatch.friendDiff){
 					topMatch.name = friends[i].name;
 					topMatch.photo = friends[i].photo;
